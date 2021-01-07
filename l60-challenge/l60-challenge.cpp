d845061c4cd9243c5469c3aa64691bc31a02050d
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-01-08 02:39:40 +0300

    Added first part of the logic

--- l60-challenge/l60-challenge.cpp
+++ l60-challenge/l60-challenge.cpp
@@ -5,6 +5,21 @@ using namespace std;
 
 int main()
 {
+    vector <int> vector1;
+    vector <int> vector2;
 
+    vector1.push_back(10);
+    vector1.push_back(20);
+
+    cout<<"First element of vector1: "<<vector1.at(0)<<endl;
+    cout<<"Second element of vector1: "<<vector1.at(1)<<endl;
+
+    vector2.push_back(100);
+    vector2.push_back(200);
+
+    cout<<"First element of vector2: "<<vector2.at(0)<<endl;
+    cout<<"Second element of vector2: "<<vector2.at(1)<<endl;
+
+    cout<<endl;
     return 0;
 }
