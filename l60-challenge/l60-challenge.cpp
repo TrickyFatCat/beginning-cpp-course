934876c806e7d80bf797f6772fb6855eb67498c5
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-01-08 02:45:55 +0300

    Adde vector_2d

--- l60-challenge/l60-challenge.cpp
+++ l60-challenge/l60-challenge.cpp
@@ -20,6 +20,16 @@ int main()
     cout<<"First element of vector2: "<<vector2.at(0)<<endl;
     cout<<"Second element of vector2: "<<vector2.at(1)<<endl;
 
+    vector <vector<int>> vector_2d;
+
+    vector2d.push_back(vector1);
+    vector2d.push_back(vector2);
+
+    cout<<"First element of vector_2d: "<<vector_2d.at(0).at(0)<<endl;
+    cout<<"Second element of vector_2d: "<<vector_2d.at(0).at(1)<<endl;
+    cout<<"Third element of vector_2d: "<<vector_2d.at(1).at(0)<<endl;
+    cout<<"Fourth element of vector_2d: "<<vector_2d.at(1).at(1)<<endl;
+
     cout<<endl;
     return 0;
 }
