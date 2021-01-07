d381a6c161322df4d813e5b055fb5996751c5405
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-01-07 23:20:36 +0300

    Added the logic

---
+++ l34-challenge/l34-challenge.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int favourite_number{};
+
+    cout<<"Welcome! Please add the number between 1 and 100."<<endl;
+    cin>>favourite_number;
+    cout<<endl;
+    cout<<"Amazing! That's my favourite numbertoo!"<<endl;
+    cout<<"No, really! "<<favourite_number<<" is my favourite number!"<<endl;
+
+    return 0;
+}
