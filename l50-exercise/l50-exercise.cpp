f1a39a50aecfc6b1e6f882eb2d3662c8c73fcde5
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-01-08 00:53:40 +0300

    Added cost calculations

--- l50-exercise/l50-exercise.cpp
+++ l50-exercise/l50-exercise.cpp
@@ -11,7 +11,9 @@ int main()
     cin>>number_of_rooms;
 
     cout<<"\nEstimate for carpet cleaning service."<<endl;
-    cout<<"Number of rooms: $"<<number_of_rooms<<endl;
+    cout<<"Number of rooms: "<<number_of_rooms<<endl;
+    cout<<"Price per room: $"<<30<<endl;
+    cout<<"Cost: $" << 30 * number_of_rooms<<endl;
 
     cout<<endl;
     return 0;
