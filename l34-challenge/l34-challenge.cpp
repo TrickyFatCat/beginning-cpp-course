d66b00175c537f8b85cc16b2034ce73fb91d1f57
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-01-07 23:22:39 +0300

    Slight code polishing

--- l34-challenge/l34-challenge.cpp
+++ l34-challenge/l34-challenge.cpp
@@ -6,9 +6,8 @@ int main()
 {
     int favourite_number{};
 
-    cout<<"Welcome! Please add the number between 1 and 100."<<endl;
+    cout<<"Welcome! Please add the number between 1 and 100: ";
     cin>>favourite_number;
-    cout<<endl;
     cout<<"Amazing! That's my favourite numbertoo!"<<endl;
     cout<<"No, really! "<<favourite_number<<" is my favourite number!"<<endl;
 
