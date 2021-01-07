3cad41c0dd40ccb3a172260bb3410371f8631de2
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-01-08 00:50:54 +0300

    Added welcome message and query

--- l50-exercise/l50-exercise.cpp
+++ l50-exercise/l50-exercise.cpp
@@ -4,5 +4,15 @@ using namespace std;
 
 int main()
 {
+    cout<<"Hello, welcome to Frank's Carpet Cleaning Service."<<endl;
+    cout<<"\nHow many rooms would you like cleaned? ";
+
+    int number_of_rooms {0};
+    cin>>number_of_rooms;
+
+    cout<<"\nEstimate for carpet cleaning service."<<endl;
+    cout<<"Number of rooms: $"<<number_of_rooms<<endl;
+
+    cout<<endl;
     return 0;
 }
