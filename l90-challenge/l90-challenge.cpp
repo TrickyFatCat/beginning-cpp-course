763ae66659c230597fa00d86e7c9eaec0295b44b
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-01-08 17:57:20 +0300

    Implemented menu text

--- l90-challenge/l90-challenge.cpp
+++ l90-challenge/l90-challenge.cpp
@@ -5,6 +5,31 @@ using namespace std;
 
 int main()
 {
+    vector <int> numbers {};
+    int smallest {};
+    int largest {};
+    int mean {};
+    char option {};
+
+    cout<<"Welcome to the Mean Calculator 1.0!"<<endl;
+
+    do
+    {
+        cout<<"Please, choose the option:"<<endl;
+        cout<<"=========================="<<endl;
+        cout<<"A - add number to the set"<<endl;
+        cout<<"P - print all numbers in the set"<<endl;
+        cout<<"C - clean set"<<endl;
+        cout<<"M - calculate and print mean"<<endl;
+        cout<<"S - find and print the smallest number"<<endl;
+        cout<<"L - find and print the largest number"<<endl;
+        cout<<"Q - Quit the program"<<endl;
+        cout<<"=========================="<<endl;
+
+        cin>>option;
+    } while (option != 'Q' && option != 'q');
+    
+
     cout<<endl;
     return 0;
 }
