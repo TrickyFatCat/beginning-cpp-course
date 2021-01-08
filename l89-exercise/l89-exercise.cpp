fa5558d3c730a58b5dc3c77fc41d8fd1020af124
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-01-08 17:24:18 +0300

    Added query logic

--- l89-exercise/l89-exercise.cpp
+++ l89-exercise/l89-exercise.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
+    int num_items {};
+    vector <int> data {};
+
+    cout<<"How many data items do you have? ";
+    cin>>num_items;
+
+    for (int i{1}; i <= num_items; i++)
+    {
+        int data_item {};
+        cout<<"Enter data item "<<i<<": ";
+        cin>>data_item;
+        data.push_back(data_item);
+    }
+
+    cout<<endl;
     return 0;
 }
