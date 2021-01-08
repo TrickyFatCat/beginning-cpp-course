c106130a00894d634b7c7f47b469eda214fbb207
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-01-08 14:33:12 +0300

    Added main logic of the program

--- l68-exercise/l68-exercise.cpp
+++ l68-exercise/l68-exercise.cpp
@@ -4,5 +4,22 @@ using namespace std;
 
 int main()
 {
+    int total {};
+    int num1 {}, num2 {}, num3 {};
+    const int count {3};
+
+    cout<<"Enter 3 integers separated by spaces: ";
+    cin>>num1>>num2>>num3;
+
+    total = num1 + num2 + num3;
+
+    double average {0.0};
+    average = total/count;
+
+    cout<<"The numbers were: "<<num1<<", "<<num2<<", "<<num3<<endl;
+    cout<<"The sum of these numbers is: "<<total<<endl;
+    cout<<"The average of the numbers is: "<<average<<endl;
+
+    cout<<endl;
     return 0;
 }
