2d72e61f6f046ea35e0d81eaa429d4592786a117
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-01-08 03:14:59 +0300

    Added new functionality

--- l60-challenge/l60-challenge.cpp
+++ l60-challenge/l60-challenge.cpp
@@ -7,15 +7,14 @@ int main()
 {
     vector <int> vector1;
     vector <int> vector2;
-    int new_element_one{};
-    int new_element_two{};
+    int new_element{};
 
     cout<<"Please, enter the first element of vector1: ";
-    cin>>new_element_one;
-    vector1.push_back(new_element_one);
+    cin>>new_element;
+    vector1.push_back(new_element);
     cout<<"Please, enter the first element of vector1: ";
-    cin>>new_element_two;
-    vector1.push_back(new_element_two);
+    cin>>new_element;
+    vector1.push_back(new_element);
     
     cout<<"================================================"<<endl;
     cout<<"First element of vector1: "<<vector1.at(0)<<endl;
@@ -24,11 +23,11 @@ int main()
     cout<<endl;
 
     cout<<"Please, enter the first element of vector2: ";
-    cin>>new_element_one;
-    vector2.push_back(new_element_one);
+    cin>>new_element;
+    vector2.push_back(new_element);
     cout<<"Please, enter the first element of vector2: ";
-    cin>>new_element_two;
-    vector2.push_back(new_element_two);
+    cin>>new_element;
+    vector2.push_back(new_element);
 
     cout<<"================================================"<<endl;
     cout<<"First element of vector2: "<<vector2.at(0)<<endl;
@@ -50,6 +49,14 @@ int main()
     cout<<"Second element of vector_2d: "<<vector_2d.at(0).at(1)<<endl;
     cout<<"Third element of vector_2d: "<<vector_2d.at(1).at(0)<<endl;
     cout<<"Fourth element of vector_2d: "<<vector_2d.at(1).at(1)<<endl;
+    cout<<"================================================"<<endl;
+
+    cout<<"\nPlease, enter new value for the first element in vector1: ";
+    cin>>new_element;
+    vector1.at(0) = new_element;
+    
+    cout<<"================================================"<<endl;
+    cout<<"First element of vector1: "<<vector1.at(0)<<endl;
     cout<<"================================================"<<endl;
 
     cout<<endl;
