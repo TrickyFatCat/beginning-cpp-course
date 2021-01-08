fced15fc238f454e1fd5ce1a28de39c37579170c
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-01-08 03:03:34 +0300

    Added new functionalty

--- l60-challenge/l60-challenge.cpp
+++ l60-challenge/l60-challenge.cpp
@@ -7,28 +7,50 @@ int main()
 {
     vector <int> vector1;
     vector <int> vector2;
+    int new_element_one{};
+    int new_element_two{};
 
-    vector1.push_back(10);
-    vector1.push_back(20);
-
+    cout<<"Please, enter the first element of vector1: ";
+    cin>>new_element_one;
+    vector1.push_back(new_element_one);
+    cout<<"Please, enter the first element of vector1: ";
+    cin>>new_element_two;
+    vector1.push_back(new_element_two);
+    
+    cout<<"================================================"<<endl;
     cout<<"First element of vector1: "<<vector1.at(0)<<endl;
     cout<<"Second element of vector1: "<<vector1.at(1)<<endl;
+    cout<<"================================================"<<endl;
+    cout<<endl;
 
-    vector2.push_back(100);
-    vector2.push_back(200);
+    cout<<"Please, enter the first element of vector2: ";
+    cin>>new_element_one;
+    vector2.push_back(new_element_one);
+    cout<<"Please, enter the first element of vector2: ";
+    cin>>new_element_two;
+    vector2.push_back(new_element_two);
 
+    cout<<"================================================"<<endl;
     cout<<"First element of vector2: "<<vector2.at(0)<<endl;
     cout<<"Second element of vector2: "<<vector2.at(1)<<endl;
+    cout<<"================================================"<<endl;
+    cout<<endl;
 
+    cout<<"Creating vector_2d..."<<endl;
     vector <vector<int>> vector_2d;
+    cout<<"Done!"<<endl;
 
+    cout<<"Adding vector1 and vector2 to vector_2d..."<<endl;
     vector_2d.push_back(vector1);
     vector_2d.push_back(vector2);
+    cout<<"Done!"<<endl;
 
+    cout<<"================================================"<<endl;
     cout<<"First element of vector_2d: "<<vector_2d.at(0).at(0)<<endl;
     cout<<"Second element of vector_2d: "<<vector_2d.at(0).at(1)<<endl;
     cout<<"Third element of vector_2d: "<<vector_2d.at(1).at(0)<<endl;
     cout<<"Fourth element of vector_2d: "<<vector_2d.at(1).at(1)<<endl;
+    cout<<"================================================"<<endl;
 
     cout<<endl;
     return 0;
