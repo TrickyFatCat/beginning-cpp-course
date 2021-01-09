70ff0059eb6e58b4642740a8621c3754983bf2db
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-01-09 18:31:51 +0300

    Added logic for calculating area of a circle

--- l103-exercise/l103-exercise.cpp
+++ l103-exercise/l103-exercise.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+const double pi {3.14159};
+
+double calc_area_circle(double radius)
+{
+    return pi * pow(radius, 2);
+}
+
+void process_area_calculation()
+{
+    double radius {};
+
+    cout<<"Please, enter the radius of the circle: ";
+    cin>>radius;
+
+    cout<<"The area of the circle with radius " << radius << " is " << calc_area_circle(radius)<<endl;
+}
+
 int main()
 {
+    cout<<"Welcome to the CilinderVolumeCalc v1.0."<<endl;
+    process_area_calculation();
     cout<<endl;
     return 0;
 }
