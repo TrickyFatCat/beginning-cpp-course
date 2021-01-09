35fe48820ad72796d22e4d53fad972bfbd971296
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-01-09 18:50:09 +0300

    Added volume of a cylinder calculations

--- l103-exercise/l103-exercise.cpp
+++ l103-exercise/l103-exercise.cpp
@@ -10,6 +10,11 @@ double calc_area_circle(double radius)
     return pi * pow(radius, 2);
 }
 
+double calc_volume_cylinder(double radius, double height)
+{
+    return calc_area_circle(radius) * height;
+}
+
 void process_area_calculation()
 {
     double radius {};
@@ -20,10 +25,26 @@ void process_area_calculation()
     cout<<"The area of the circle with radius " << radius << " is " << calc_area_circle(radius)<<endl;
 }
 
+
+void process_volume_calc()
+{
+    double radius {};
+    double height {};
+
+    cout<<"\nEnter the radius of the cylinder: ";
+    cin>>radius;
+    
+    cout<<"\nEnter the height of the cylinder: ";
+    cin>>height;
+
+    cout<<"\nThe volume of the cylinder with radius "<<radius<<" and height "<< height <<"is "<<calc_volume_cylinder(radius, height)<<endl;
+}
+
 int main()
 {
-    cout<<"Welcome to the CilinderVolumeCalc v1.0."<<endl;
-    process_area_calculation();
+    cout<<"Welcome to the CylinderVolumeCalc v1.0."<<endl;
+    // process_area_calculation();
+    process_volume_calc();
     cout<<endl;
     return 0;
 }
