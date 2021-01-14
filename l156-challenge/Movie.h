8249ff028519f0e305a84537476f8a1f76f9580f
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-01-14 23:49:14 +0300

    Fix Movie.h content

--- l156-challenge/Movie.h
+++ l156-challenge/Movie.h
@@ -10,12 +10,12 @@ private:
     std::string rating;
     int watch_count;
 public:
+    Movie::Movie(std::string name, std::string rating, int watching_count = 0);
+    
     int increase_watch_count();
     std::string get_name(const Movie &movie) const;
     std::string get_rating(const Movie &movie) const;
     int get_watch_count(const Movie &movie) const;
-
-    Movie::Movie(std::string name, std::string rating, int watching_count = 0);
 };
 
 #endif
