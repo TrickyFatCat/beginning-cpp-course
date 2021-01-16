3c30259df988475044fc671a9dfc3cf98a04df23
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-01-17 02:12:47 +0300

    Rework Menu to substitute MoviesManager

--- l156-challenge/Menu.cpp
+++ l156-challenge/Menu.cpp
@@ -21,7 +21,7 @@ char Menu::get_query() const
 
 void Menu::draw_menu() const
 {
-    cout<<"Welcome to the MovieList v1.0\n"
+    cout<<"Welcome to the MovieList v1.0.\n"
         <<"A - to add movie to the list\n"
         <<"D - to delete movie from the list\n"
         <<"I - to increase watch count of a movie\n"
@@ -31,7 +31,7 @@ void Menu::draw_menu() const
         <<"Q - to quit the program\n";
 }
 
-void Menu::process_query(MoviesManager &manager, Movies &movies)
+void Menu::process_query(Movies &movies)
 {
     cout<<"Please, enter the command: ";
     cin>>query;
@@ -41,27 +41,27 @@ void Menu::process_query(MoviesManager &manager, Movies &movies)
     {
     case 'A':
     case 'a':
-        process_add_movie(manager, movies);
+        process_add_movie(movies);
         break;
     
     case 'D':
     case 'd':
-        process_delete_movie(manager, movies);
+        process_delete_movie(movies);
         break;
 
     case 'I':
     case 'i':
-        process_watch_count_increase(manager, movies);
+        process_watch_count_increase(movies);
         break;
 
     case 'M':
     case 'm':
-        process_show_movie_data(manager, movies);
+        process_show_movie_data(movies);
         break;
 
     case 'L':
     case 'l':
-        process_show_all_movies(manager, movies);
+        process_show_all_movies(movies);
         break;
 
     case 'H':
@@ -80,7 +80,7 @@ void Menu::process_query(MoviesManager &manager, Movies &movies)
     }
 }
 
-void Menu::process_add_movie(MoviesManager &manager, Movies &movies)
+void Menu::process_add_movie(Movies &movies)
 {
     string movie_name {};
     string rating {};
@@ -89,28 +89,117 @@ void Menu::process_add_movie(MoviesManager &manager, Movies &movies)
     movie_name = inquire_movie_name();
     cout<<"Enter movie rating: ";
     cin>>rating;
+    cin.ignore();
     cout<<"Enter watch count: ";
     cin>>watch_count;
+    cin.ignore();
+    
+    bool result = movies.add_movie(movie_name, rating, watch_count);
 
-    manager.add_movie(movies, movie_name, rating, watch_count);
+    if (result)
+    {
+        cout<<"\nDone! New movie added.\n";
+        show_movie_data(movies, movie_name);
+    }
+    else
+    {
+        cout<<"This movie is already in the list.\n";
+        show_movie_data(movies, movie_name);
+    }
 }
 
-void Menu::process_delete_movie(MoviesManager &manager, Movies &movies)
+void Menu::process_delete_movie(Movies &movies)
 {
-    manager.delete_movie(movies, inquire_movie_name());
+    if(movies.is_list_empty())
+    {
+        show_error_list_is_empty();
+        return;
+    }
+    
+    string movie_name = inquire_movie_name();
+    bool result = movies.delete_movie(movie_name);
+
+    if(result)
+    {
+        cout<<movie_name<<" was deleted from the list.\n";
+    }
+    else
+    {
+        show_error_movie_not_found(movie_name);
+    }
 }
 
-void Menu::process_watch_count_increase(MoviesManager &manager, Movies &movies)
+void Menu::process_watch_count_increase(Movies &movies)
 {
-    manager.increase_watch_count(movies, inquire_movie_name());
+    if(movies.is_list_empty())
+    {
+        show_error_list_is_empty();
+        return;
+    }
+
+    string movie_name = inquire_movie_name();
+    bool result = movies.increase_watch_count(movie_name);
+
+    if(result)
+    {
+        cout<<movie_name<<" watch count was increased.\n";
+    }
+    else
+    {
+        show_error_movie_not_found(movie_name);
+    }
 }
 
-void Menu::process_show_movie_data(MoviesManager &manager, Movies &movies)
+void Menu::process_show_movie_data(Movies &movies)
 {
-    manager.show_movie_data(movies, inquire_movie_name());
+    if(movies.is_list_empty())
+    {
+        show_error_list_is_empty();
+        return;
+    }
+
+    string movie_name = inquire_movie_name();
+
+    show_movie_data(movies, movie_name);
+ }
+
+void Menu::process_show_all_movies(Movies &movies)
+{
+    if(movies.is_list_empty())
+    {
+        show_error_list_is_empty();
+        return;
+    }
+
+    for(const auto &movie: *movies.get_movies_list())
+    {
+        show_movie_data(movie);
+    } 
 }
 
-void Menu::process_show_all_movies(MoviesManager &manager, Movies &movies)
+void Menu::show_movie_data(const Movie &movie) const
 {
-    manager.show_all_movies(movies);
+    cout<<movie.get_name()<<" | "<<movie.get_rating()<<" | "<<movie.get_watch_count()<<endl;
+}
+
+void Menu::show_movie_data(Movies &movies, string movie_name)
+{
+    for(const auto &movie: *movies.get_movies_list())
+    {
+        if (movie.get_name() == movie_name)
+        {
+            show_movie_data(movie);
+        }
+    }
+}
+
+void Menu::show_error_list_is_empty()
+{
+    cout<<"The list is empty.\n";
+}
+
+void Menu::show_error_movie_not_found(string movie_name) const
+{
+    cout<<"Movie "<<movie_name<<" not found in the list.\n";
+
 }
--- l156-challenge/Menu.h
+++ l156-challenge/Menu.h
@@ -12,15 +12,19 @@ class Menu
 private:
     char query {};
     string inquire_movie_name();
-    void process_add_movie(MoviesManager &manager, Movies &movies);
-    void process_delete_movie(MoviesManager &manager, Movies &movies);
-    void process_watch_count_increase(MoviesManager &manager, Movies &movies);
-    void process_show_movie_data(MoviesManager &manager, Movies &movies);
-    void process_show_all_movies(MoviesManager &manager, Movies &movies);
+    void process_add_movie(Movies &movies);
+    void process_delete_movie(Movies &movies);
+    void process_watch_count_increase(Movies &movies);
+    void process_show_movie_data(Movies &movies);
+    void process_show_all_movies(Movies &movies);
+    void show_movie_data(const Movie &movie) const;
+    void show_movie_data(Movies &movies, string movie_name);
+    void show_error_list_is_empty();
+    void show_error_movie_not_found(string movie_name) const;
 public:
     char get_query() const;
     void draw_menu() const;
-    void process_query(MoviesManager &manager, Movies &movies);
+    void process_query(Movies &movies);
 };
 
 #endif
