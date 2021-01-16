27b5b4bd63c080ceea6fe6e882ee7bbad8a814a8
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-01-17 00:03:02 +0300

    Create new class Menu

---
+++ l156-challenge/Menu.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <string>
+#include "Menu.h"
+#include "MoviesManager.h"
+
+char Menu::get_query() const
+{
+    return query;
+}
+
+void Menu::draw_menu() const
+{
+    cout<<"Welcome to the MovieList v1.0\n"
+        <<"A - to add movie to the list\n"
+        <<"D - to delete movie from the list\n"
+        <<"I - to increase watch count of a movie\n"
+        <<"M - to show informasion about a movie\n"
+        <<"L - to print the list of movies\n"
+        <<"Q - to quit the program\n";
+}
+
+void Menu::process_query(MoviesManager &manager, Movies &movies)
+{
+    cout<<"Please, enter the command: ";
+    cin>>query;
+    cin.ignore();
+
+    switch (query)
+    {
+    case 'A':
+    case 'a':
+        process_add_movie(manager, movies);
+        break;
+    
+    case 'D':
+        process_delete_movie(manager, movies);
+    case 'd':
+        break;
+
+    case 'I':
+    case 'i':
+        break;
+
+    case 'M':
+    case 'm':
+        break;
+
+    case 'L':
+    case 'l':
+        break;
+
+    case 'Q':
+    case 'q':
+        break;
+    
+    default:
+        break;
+    }
+}
+
+void Menu::process_add_movie(MoviesManager &manager, Movies &movies)
+{
+    string movie_name {};
+    string rating {};
+    int watch_count {0};
+
+    cout<<"\nEnter movie name: ";
+    getline(cin, movie_name);
+    cout<<"\nEnter movie rating: ";
+    cin>>rating;
+    cout<<"\nEnter watch count: ";
+    cin>>watch_count;
+
+    manager.add_movie(movies, movie_name, rating, watch_count);
+}
+
+void Menu::process_delete_movie(MoviesManager &manager, Movies &movies)
+{
+    string movie_name {};
+
+    cout<<"\nEnter movie name: ";
+    getline(cin, movie_name);
+
+    manager.delete_movie(movies, movie_name);
+}
---
+++ l156-challenge/Menu.h
@@ -0,0 +1,22 @@
+#ifndef _MENU_H_
+#define _MENU_H_
+
+#include <string>
+#include "MoviesManager.h"
+#include "Movies.h"
+
+using namespace std;
+
+class Menu
+{
+private:
+    char query {};
+    void process_add_movie(MoviesManager &manager, Movies &movies);
+    void process_delete_movie(MoviesManager &manager, Movies &movies);
+public:
+    char get_query() const;
+    void draw_menu() const;
+    void process_query(MoviesManager &manager, Movies &movies);
+};
+
+#endif
