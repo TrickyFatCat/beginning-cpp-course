81b3f479518da6163c4fd95177de5fc155c5e8ef
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-01-17 02:17:02 +0300

    Implement the main logic of the program

--- l156-challenge/l156-challenge.cpp
+++ l156-challenge/l156-challenge.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include "Movie.h"
+#include "Movies.h"
+#include "MoviesManager.h"
+#include "Menu.h"
 
 using namespace std;
 
 int main()
 {
+    Movies my_movies;
+    Menu menu;
+    menu.draw_menu();
 
+    do
+    {
+        menu.process_query(my_movies);
+    } while (menu.get_query() != 'Q' && menu.get_query() != 'q');
     cout<<endl;
     return 0;
 }
