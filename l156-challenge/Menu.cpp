97579660bca0e01cb40f7cbf15c1f178df979b9c
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-01-17 00:43:53 +0300

    Add new functionality for showing all lis and refactor some code

--- l156-challenge/Menu.cpp
+++ l156-challenge/Menu.cpp
@@ -3,6 +3,17 @@
 #include "Menu.h"
 #include "MoviesManager.h"
 
+string Menu::inquire_movie_name()
+{
+   string movie_name {};
+
+   cout<<"Enter movie name: ";
+   getline(cin, movie_name);
+
+   return movie_name;
+}
+
+
 char Menu::get_query() const
 {
     return query;
@@ -16,6 +27,7 @@ void Menu::draw_menu() const
         <<"I - to increase watch count of a movie\n"
         <<"M - to show informasion about a movie\n"
         <<"L - to print the list of movies\n"
+        <<"H - to show help\n"
         <<"Q - to quit the program\n";
 }
 
@@ -49,6 +61,11 @@ void Menu::process_query(MoviesManager &manager, Movies &movies)
 
     case 'L':
     case 'l':
+        process_show_all_movies(manager, movies);
+        break;
+
+    case 'H':
+    case 'h':
         break;
 
     case 'Q':
@@ -65,12 +82,11 @@ void Menu::process_add_movie(MoviesManager &manager, Movies &movies)
     string movie_name {};
     string rating {};
     int watch_count {0};
-
-    cout<<"\nEnter movie name: ";
-    getline(cin, movie_name);
-    cout<<"\nEnter movie rating: ";
+    
+    movie_name = inquire_movie_name();
+    cout<<"Enter movie rating: ";
     cin>>rating;
-    cout<<"\nEnter watch count: ";
+    cout<<"Enter watch count: ";
     cin>>watch_count;
 
     manager.add_movie(movies, movie_name, rating, watch_count);
@@ -78,30 +94,20 @@ void Menu::process_add_movie(MoviesManager &manager, Movies &movies)
 
 void Menu::process_delete_movie(MoviesManager &manager, Movies &movies)
 {
-    string movie_name {};
-
-    cout<<"Enter movie name: ";
-    getline(cin, movie_name);
-
-    manager.delete_movie(movies, movie_name);
+    manager.delete_movie(movies, inquire_movie_name());
 }
 
 void Menu::process_watch_count_increase(MoviesManager &manager, Movies &movies)
 {
-    string movie_name {};
-
-    cout<<"Enter movie name: ";
-    getline(cin, movie_name);
-
-    manager.increase_watch_count(movies, movie_name);
+    manager.increase_watch_count(movies, inquire_movie_name());
 }
 
 void Menu::process_show_movie_data(MoviesManager &manager, Movies &movies)
 {
-    string movie_name {};
+    manager.show_movie_data(movies, inquire_movie_name());
+}
 
-    cout<<"Enter movie name: ";
-    getline(cin, movie_name);
-    
-    manager.show_movie_data(movies, movie_name);
+void Menu::process_show_all_movies(MoviesManager &manager, Movies &movies)
+{
+    manager.show_all_movies(movies);
 }
--- l156-challenge/Menu.h
+++ l156-challenge/Menu.h
@@ -11,10 +11,12 @@ class Menu
 {
 private:
     char query {};
+    string inquire_movie_name();
     void process_add_movie(MoviesManager &manager, Movies &movies);
     void process_delete_movie(MoviesManager &manager, Movies &movies);
     void process_watch_count_increase(MoviesManager &manager, Movies &movies);
     void process_show_movie_data(MoviesManager &manager, Movies &movies);
+    void process_show_all_movies(MoviesManager &manager, Movies &movies);
 public:
     char get_query() const;
     void draw_menu() const;
