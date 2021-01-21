691f585cf380e4ad850dfd635d217ae416faeb16
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-01-21 23:46:42 +0300

    Fix functions overload

--- l180-challenge/Accounts_Util.h
+++ l180-challenge/Accounts_Util.h
@@ -11,8 +11,8 @@ void deposit_all(vector<Account> &accounts, double amount);
 void withdraw_all(vector<Account> &account, double amount);
 
 // Utility functions for Savings_Account class
-void display_all(const vector<Account> &accounts);
-void deposit_all(vector<Account> &accounts, double amount);
-void withdraw_all(vector<Account> &accounts, double amount);
+void display_all(const vector<Savings_Account> &accounts);
+void deposit_all(vector<Savings_Account> &accounts, double amount);
+void withdraw_all(vector<Savings_Account> &account, double amount);
 
 #endif
