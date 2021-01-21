2b38e3394267ebf80ff40c3d61bce6ef7135a5db
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-01-21 22:30:24 +0300

    Implement temporary debug functions

---
+++ l180-challenge/Accounts_Util.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include "Account.h"
+#include "Savings_Account.h"
+#include "Accounts_Util.h"
+
+using namespace std;
+
+// Utility functions for Account class
+void display_all(const vector<Account> &accounts)
+{
+    for(const auto &account: accounts)
+    {
+        cout<<account<<endl;
+    }
+}
+
+void deposit_all(vector<Account> &accounts, double amount)
+{
+    for(auto &account: accounts)
+    {
+        if(account.deposit(amount))
+        {
+            cout<<"Deposited "<<amount<<" to "<<account<<endl;
+        }
+        else
+        {
+            cout<<"Failed deposit of "<<amount<<" to "<<account<<endl;
+        }
+    }
+}
+
+void withdraw_all(vector<Account> &accounts, double amount)
+{
+    for(auto &account: accounts)
+    {
+        if(account.withdraw(amount))
+        {
+            cout<<"Withdrew "<<amount<<" from "<<account<<endl;
+        }
+        else
+        {
+            cout<<"Failed withdrawal of"<<amount<<" from "<<account<<endl;
+        }
+    }
+}
+
+// Utility functions for Savings_Account class
+void display_all(const vector<Savings_Account> &accounts)
+{
+    for(const auto &account: accounts)
+    {
+        cout<<account<<endl;
+    }
+}
+
+void deposit_all(vector<Savings_Account> &accounts, double amount)
+{
+    for(auto &account: accounts)
+    {
+        if(account.deposit(amount))
+        {
+            cout<<"Deposited "<<amount<<" to "<<account<<endl;
+        }
+        else
+        {
+            cout<<"Failed deposit of "<<amount<<" to "<<account<<endl;
+        }
+    }
+}
+
+void withdraw_all(vector<Savings_Account> &accounts, double amount)
+{
+    for(auto &account: accounts)
+    {
+        if(account.withdraw(amount))
+        {
+            cout<<"Withdrew "<<amount<<" from "<<account<<endl;
+        }
+        else
+        {
+            cout<<"Failed withdrawal of"<<amount<<" from "<<account<<endl;
+        }
+    }
+}
---
+++ l180-challenge/Accounts_Util.h
@@ -0,0 +1,18 @@
+#ifndef _ACCOUNTS_UTIL_H_
+#define _ACCOUNTS_UTIL_H_
+
+#include <vector>
+#include "Account.h"
+#include "Savings_Account.h"
+
+// Utility functions for Account class
+void display_all(const vector<Account> &accounts);
+void deposit_all(vector<Account> &accounts, double amount);
+void withdraw_all(vector<Account> &account, double amount);
+
+// Utility functions for Savings_Account class
+void display_all(const vector<Account> &accounts);
+void deposit_all(vector<Account> &accounts, double amount);
+void withdraw_all(vector<Account> &accounts, double amount);
+
+#endif
