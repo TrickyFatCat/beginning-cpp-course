1d4e29721291cf11818eefdf68a65991311683e2
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-01-21 20:10:47 +0300

    Implement ostream operator << overloading

--- l180-challenge/Account.cpp
+++ l180-challenge/Account.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <string>
 #include "Account.h"
 
@@ -35,4 +36,10 @@ bool Account::withdraw(double amount)
 double Account::get_balance() const
 {
     return balance;
+}
+
+std::ostream &operator<<(std::ostream &os, const Account &account)
+{
+    os<<"[Account: "<<account.name<<" | "<<account.balance<<"]";
+    return os;
 }
