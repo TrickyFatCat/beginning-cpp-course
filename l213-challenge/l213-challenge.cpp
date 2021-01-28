c0fa00c1110c3ae265a2f571741db87659281282
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-01-29 00:24:53 +0300

    Implement IllegalBalanceException handling

--- l213-challenge/l213-challenge.cpp
+++ l213-challenge/l213-challenge.cpp
@@ -20,18 +20,33 @@ int main()
     try
     {
         Account *sa_0 = new Savings_Account{"Savings Account 0", -1000.0};
-        Account *sa_1 = new Savings_Account{"Savings Account 1", 1000.0};
-        Account *sa_2 = new Savings_Account{"Savings Account 2", 2000.0, 0.1};
-  
         savings_accounts.push_back(sa_0);
-        savings_accounts.push_back(sa_1);
-        savings_accounts.push_back(sa_2);
-
     }
     catch(const IllegalBalanceException &exception)
     {
         cerr<<exception.what()<<endl;
     }
+
+    try
+    {
+        Account *sa_1 = new Savings_Account{"Savings Account 1", 1000.0};
+        savings_accounts.push_back(sa_1);
+    }
+    catch(const IllegalBalanceException &exception)
+    {
+        cerr<<exception.what()<<endl;
+    }
+
+    try
+    {
+        Account *sa_2 = new Savings_Account{"Savings Account 2", 2000.0, 0.1};
+        savings_accounts.push_back(sa_2);
+    }
+    catch(const IllegalBalanceException &exception)
+    {
+        cerr<<exception.what()<<endl;
+    }
+
     display_all(savings_accounts);
     deposit_all(savings_accounts, 100);
     withdraw_all(savings_accounts, 200);
