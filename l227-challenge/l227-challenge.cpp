4f6ce5fcef12d0378cf140aaced110e4487734fd
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-02-01 23:57:15 +0300

    Add base score calculations

--- l227-challenge/l227-challenge.cpp
+++ l227-challenge/l227-challenge.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
@@ -19,6 +20,8 @@ int main()
     string answer_key {};
     string student_name {};
     string student_answer {};
+    int student_score{0};
+    vector<int> all_scores {};
     int line {1};
     
     
@@ -41,10 +44,30 @@ int main()
         else
         {
             student_answer = data;
-            cout << student_name << " | " << student_answer << endl;
+            for(size_t i = 0; i < student_answer.length(); ++i)
+            {
+                if(student_answer.at(i) == answer_key.at(i))
+                {
+                    student_score++;
+                }
+            }
+            cout << student_name << " | " << student_answer << " | " << student_score<< endl;
+            all_scores.push_back(student_score);
+            student_score= 0;
             line++;
         }
     }
+
+    int sum {0};
+    for(const int &score: all_scores)
+    {
+        sum += score;
+    }
+
+    double average_score{};
+    average_score= static_cast<double>(sum) / all_scores.size();
+
+    cout << "Average score: " << average_score;
 
     in_file.close();
 
