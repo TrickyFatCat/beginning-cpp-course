3bc4e19531548c74ca4fb5d6194e0e1ff2cb0599
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-02-01 23:43:57 +0300

    Add basic data printing

--- l227-challenge/l227-challenge.cpp
+++ l227-challenge/l227-challenge.cpp
@@ -5,21 +5,49 @@ using namespace std;
 
 int main()
 {
-    ifstream in_file;
-    string data;
+    ifstream in_file {};
 
-    in_file.open("../files/responses.txt");
+    in_file.open("./files/responses.txt"); // One dot for debug inside VsCode, two dots for binary. (Linux)
 
     if(!in_file)
     {
         cerr << "Can't open the file." << endl;
         return 1;
     }
+
+    string data;
+    string answer_key {};
+    string student_name {};
+    string student_answer {};
+    int line {1};
+    
     
     while(in_file >> data)
     {
-        cout<<data<<endl;
+        if(line == 1)
+        {
+            answer_key = data;
+            cout << "Answer key: " << answer_key << endl;
+            line++;
+            continue;
+        }
+
+        if(line % 2 == 0)
+        {
+            student_name = data;
+            line++;
+            continue;
+        }
+        else
+        {
+            student_answer = data;
+            cout << student_name << " | " << student_answer << endl;
+            line++;
+        }
     }
+
+    in_file.close();
+
     cout<<endl;
     return 0;
 }
