f26e0b5dfaf5df9e2903dbf6073264522cbdde22
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-02-02 23:11:21 +0300

    Add checking file

--- l229-challenge/l229-challenge.cpp
+++ l229-challenge/l229-challenge.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <fstream>
+#include "utility.h"
 
-main()
+int main()
 {
+    std::ifstream in_file {};
+
+    in_file.open("./files/romeoandjuliet.txt"); // One dot for debug inside VScode, two dots for executeable
+
+    if(!in_file)
+    {
+        std::cout << "Error! Couldn't open the file." << std::endl;
+        return 1;
+    }
+
+    std::cout << "Hello!" << std::endl;
+
     return 0;
 }
