7c4325ae9947eecee9958df92c02a1aecaaf00d8
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-02-03 23:23:29 +0300

    Add opening and creation of files

--- l233-challenge/l233-challenge.cpp
+++ l233-challenge/l233-challenge.cpp
@@ -3,5 +3,25 @@
 
 int main()
 {
+    std::ifstream in_file {};
+    std::ofstream out_file {};
+
+    in_file.open("./files/romeoandjuliet.txt");
+    
+    if(!in_file)
+    {
+        std::cerr << "Error! Couldn't open the file." << std::endl;
+        return 1;
+    }
+
+    out_file.open("./files/romeoandjuliet_v2.txt");
+
+    if(!out_file)
+    {
+        std::cerr << "Error! Couldn't create the file." << std::endl;
+    }
+    
+
+
     return 0;
 }
